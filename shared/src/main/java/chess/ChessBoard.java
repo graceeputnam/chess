0656f1b39b7d39f3d409bf174d82cf48bc4bd865
Gracee Putnam <gracee.putnam@gmail.com>
2026-01-23 18:30:33 -0700

    cleaning up code

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -31,35 +31,18 @@ public class ChessBoard {
 
     }
 
-    /**
-     * Adds a chess piece to the chessboard
-     *
-     * @param position where to add the piece to
-     * @param piece    the piece to add
-     */
     public void addPiece(ChessPosition position, ChessPiece piece) {
         int rowIndex = position.getRow() - 1;
         int colIndex = position.getColumn() - 1;
         board[rowIndex][colIndex] = piece;
     }
 
-    /**
-     * Gets a chess piece on the chessboard
-     *
-     * @param position The position to get the piece from
-     * @return Either the piece at the position, or null if no piece is at that
-     * position
-     */
     public ChessPiece getPiece(ChessPosition position) {
         int rowIndex = position.getRow() - 1;
         int colIndex = position.getColumn() - 1;
         return board[rowIndex][colIndex];
     }
 
-    /**
-     * Sets the board to the default starting board
-     * (How the game of chess normally starts)
-     */
     public void resetBoard() {
         // clear the whole board
         for (int i = 0; i < 8; i++){
