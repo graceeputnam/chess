95b3e09319bbf48ff7580b37c4290e30e9f43e41
Gracee Putnam <gracee.putnam@gmail.com>
2026-03-11 21:56:38 -0600

    Adding createAuth tests negative and positive

--- server/src/main/java/dataaccess/DataAccessTest.java
+++ server/src/main/java/dataaccess/DataAccessTest.java
@@ -33,4 +33,20 @@ public class DataAccessTest {
         assertThrows(DataAccessException.class, () -> dao.createUser(user2));
     }
 
+    @Test
+    void createAuth_positive() throws DataAccessException {
+        var user = new UserData("alice", "password", "alice@example.com");
+        dao.createUser(user);
+        var auth = dao.createAuth("alice");
+
+        assertNotNull(auth);
+        assertEquals("alice", auth.username());
+        assertNotNull(auth.authToken());
+    }
+
+    @Test
+    void createAuth_negative_unknownUser() {
+        assertThrows(DataAccessException.class, () -> dao.createAuth("no-such-user"));
+    }
+
 }
