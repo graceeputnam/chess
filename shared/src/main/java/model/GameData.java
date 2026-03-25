6dbe51ecf4d67c9f261facddd1c63ed84da75625
Gracee Putnam <gracee.putnam@gmail.com>
2026-03-25 11:09:53 -0600

    Creating the new data files

---
+++ shared/src/main/java/model/GameData.java
@@ -0,0 +1,10 @@
+package model;
+
+import chess.ChessGame;
+
+public record GameData
+        (int gameID,
+         String gameName,
+         String whiteUsername,
+         String blackUsername,
+         ChessGame game) {}
