f3051e0d8de3443bd2c2034ef2f8548e6c684f5c
Gracee Putnam <gracee.putnam@gmail.com>
2026-04-08 15:05:33 -0600

    changing servermessage to include game, errormessage and message

--- shared/src/main/java/websocket/messages/ServerMessage.java
+++ shared/src/main/java/websocket/messages/ServerMessage.java
@@ -1,43 +1,37 @@
 package websocket.messages;
 
+import chess.ChessGame;
 import java.util.Objects;
 
-/**
- * Represents a Message the server can send through a WebSocket
- * <p>
- * Note: You can add to this class, but you should not alter the existing
- * methods.
- */
 public class ServerMessage {
-    ServerMessageType serverMessageType;
-
     public enum ServerMessageType {
         LOAD_GAME,
         ERROR,
         NOTIFICATION
     }
 
+    public ServerMessageType serverMessageType;
+    public ChessGame game;
+    public String errorMessage;
+    public String message;
+
     public ServerMessage(ServerMessageType type) {
         this.serverMessageType = type;
     }
 
     public ServerMessageType getServerMessageType() {
-        return this.serverMessageType;
+        return serverMessageType;
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof ServerMessage that)) {
-            return false;
-        }
-        return getServerMessageType() == that.getServerMessageType();
+        if (this == o) return true;
+        if (!(o instanceof ServerMessage that)) return false;
+        return serverMessageType == that.serverMessageType;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getServerMessageType());
+        return Objects.hash(serverMessageType);
     }
 }
